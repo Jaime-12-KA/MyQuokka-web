4b656ff2bc2b715a07ea4b53a3cf984a28567b06
Jaime-12-KA <seongyeonjoo@gmail.com>
2025-03-16 18:46:01 +0900

    i18n 클라이언트 및 서버 코드 단순화

--- src/i18n/client.ts
+++ src/i18n/client.ts
@@ -2,19 +2,24 @@
 
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import resourcesToBackend from 'i18next-resources-to-backend';
 import { getOptions } from '../middleware-settings';
 
+// 간소화된 버전의 i18n 클라이언트 설정
 i18next
   .use(initReactI18next)
-  .use(resourcesToBackend((language, namespace) => 
-    import(`../../public/locales/${language}/${namespace}.json`)
-  ))
   .init({
     ...getOptions(),
     lng: undefined, // 클라이언트 측에서는 자동 감지를 사용
     detection: {
       order: ['path', 'htmlTag', 'cookie', 'navigator'],
+    },
+    resources: {
+      ko: {
+        common: require('../../public/locales/ko/common.json')
+      },
+      en: {
+        common: require('../../public/locales/en/common.json')
+      }
     }
   });
 
--- src/i18n/server.ts
+++ src/i18n/server.ts
@@ -1,16 +1,23 @@
 ﻿import { createInstance } from 'i18next';
-import resourcesToBackend from 'i18next-resources-to-backend';
 import { initReactI18next } from 'react-i18next/initReactI18next';
 import { getOptions } from '../middleware-settings';
 
+// 간소화된 버전의 i18n 서버 설정
 const initI18next = async (lng, ns) => {
   const i18nInstance = createInstance();
   await i18nInstance
     .use(initReactI18next)
-    .use(resourcesToBackend((language, namespace) => 
-      import(`../../public/locales/${language}/${namespace}.json`)
-    ))
-    .init(getOptions(lng, ns));
+    .init({
+      ...getOptions(lng, ns),
+      resources: {
+        ko: {
+          common: require('../../public/locales/ko/common.json')
+        },
+        en: {
+          common: require('../../public/locales/en/common.json')
+        }
+      }
+    });
   return i18nInstance;
 };
 
